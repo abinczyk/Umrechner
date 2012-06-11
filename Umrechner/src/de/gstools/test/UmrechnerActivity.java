f55c180bbbe83a7568583e965ff1c18c3d5f753b
abinczyk <andreas.binczyk@gmail.com>
2012-06-11 18:08:15 +0200

    bug 1

--- Umrechner/src/de/gstools/test/UmrechnerActivity.java
+++ Umrechner/src/de/gstools/test/UmrechnerActivity.java
@@ -16,6 +16,7 @@ public class UmrechnerActivity extends Activity {
 	}
 
 	public void umRechnen(View view){
+
 		TextView text = (TextView) findViewById(R.id.textView1);
 		RadioButton zoll = (RadioButton) findViewById(R.id.radio1);
 		EditText wert = (EditText) findViewById(R.id.editText1);
@@ -28,6 +29,7 @@ public class UmrechnerActivity extends Activity {
 			double cm_conv = umrechnenZollinZentimeter(d);
 			text.setText(Double.toString(cm_conv));
 		}
+	
 	}
 	
 	public void Loeschen(View view){
